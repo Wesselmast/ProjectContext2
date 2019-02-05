a4a72e439b3effaba1b09dd0a569a61794de788e
Wessel Mast <mastwessel@gmail.com>
2019-02-06 00:20:05 +0100

    FPS test

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace ContextInput {
+    public class PlayerInput : MonoBehaviour {
+        public static float Horizontal { get; private set; }
+        public static float Vertical { get; private set; }
+        public static Vector3 MouseRotation { get; private set; }
+        public static event Action OnJump = delegate { };
+        public static event Action OnShoot = delegate { };
+
+        private void Update() {
+            Horizontal = Input.GetAxis("Horizontal");
+            Vertical = Input.GetAxis("Vertical");
+            MouseRotation = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            if (Input.GetKeyDown(KeyCode.Space)) OnJump();
+            if (Input.GetKeyDown(KeyCode.Mouse0)) OnShoot();
+        }
+    }
+}
---
+++ Assets/Scripts/UIUpdater.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIUpdater : MonoBehaviour {
+
+    private Slider slider;
+
+    private void Start() {
+        slider = GetComponent<Slider>();
+    }
+
+    private void Update() {
+        slider.value = MaterialManager.GetMaterial();
+    }
+}
