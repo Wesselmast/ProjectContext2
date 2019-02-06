aaf38a0f9331d95726f7be3bc14571d166840702
Wessel Mast <mastwessel@gmail.com>
2019-02-06 14:41:54 +0100

    Changes!

---
+++ Assets/Scripts/GridSystem/FurniturePlacement.cs
@@ -0,0 +1,29 @@
+﻿using ContextInput;
+using UnityEngine;
+
+public class FurniturePlacement : MonoBehaviour {
+
+    [SerializeField] private GameObject furniturePrefab;
+    [SerializeField] private Vector3 placementOffset;
+
+    private Grid grid;
+    private Camera cam;
+
+    private void Awake() {
+        grid = FindObjectOfType<Grid>();
+        cam = Camera.main;
+        PlayerInput.OnLeftClick += Place;
+    }
+
+    private void Place() {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(cam.ScreenPointToRay(PlayerInput.MousePosition), out hitInfo)) {
+            if (hitInfo.transform == grid.transform) {
+                Instantiate(furniturePrefab, grid.GetGridPoint(hitInfo.point) + placementOffset, Quaternion.identity);
+            }
+            else if (hitInfo.collider.tag == "Furniture") {
+                Destroy(hitInfo.collider.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GridSystem/PlayerInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace ContextInput {
+
+    public enum Direction {
+        Left, Right
+    }
+
+    public class PlayerInput : MonoBehaviour {
+
+        public static event Action OnLeftClick = delegate { };
+        public static event Action OnRightClick = delegate { };
+        public static float Horizontal { get; private set; }
+        public static float Vertical { get; private set; }
+        public static event Action<Direction> Rotate = delegate { };
+        public static Vector3 MousePosition { get; private set; }
+
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.Mouse0)) OnLeftClick();
+            if (Input.GetKeyDown(KeyCode.Mouse1)) OnRightClick();
+            if (Input.GetKeyDown(KeyCode.Q)) Rotate(Direction.Left);
+            if (Input.GetKeyDown(KeyCode.E)) Rotate(Direction.Right);
+            Horizontal = Input.GetAxis("Horizontal");
+            Vertical =  Input.GetAxis("Vertical");
+            MousePosition = Input.mousePosition;
+        }
+    }
+}
