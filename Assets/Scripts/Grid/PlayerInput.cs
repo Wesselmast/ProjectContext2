a8358bff6073dcd67d76df940d9a3fb5446e984e
Wessel Mast <mastwessel@gmail.com>
2019-02-07 17:43:23 +0100

    Latest stuff

--- Assets/Scripts/Grid/PlayerInput.cs
+++ Assets/Scripts/Grid/PlayerInput.cs
@@ -21,8 +21,8 @@ namespace ContextInput {
             if (Input.GetKeyDown(KeyCode.A)) Horizontal = -1;
             else if (Input.GetKeyDown(KeyCode.D)) Horizontal = 1;
             else Horizontal = 0;
-            if (Input.GetKeyDown(KeyCode.T)) Vertical = 1;
-            else if (Input.GetKeyDown(KeyCode.G)) Vertical = -1;
+            if (Input.GetKeyDown(KeyCode.W)) Vertical = 1;
+            else if (Input.GetKeyDown(KeyCode.S)) Vertical = -1;
             else Vertical = 0;
             MousePosition = Input.mousePosition;
         }
