4d8337120ec30a1f23df847f4701f73e732d4299
Wessel Mast <mastwessel@gmail.com>
2019-02-07 11:01:51 +0100

    Temp placeable

---
+++ Assets/Scripts/Grid/CalcNotPlaceable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalcNotPlaceable : MonoBehaviour {
+
+    [SerializeField] private GameObject notPlaceable;
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+
+    }
+}
