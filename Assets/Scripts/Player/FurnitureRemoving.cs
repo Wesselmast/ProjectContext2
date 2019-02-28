d39accced7ba13e5b487ea67a8a7840ba49222eb
Wesselmast <mastwessel@gmail.com>
2019-02-28 10:59:26 +0100

    Removed placement bug, even better lighting

--- Assets/Scripts/Player/FurnitureRemoving.cs
+++ Assets/Scripts/Player/FurnitureRemoving.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 
 public class FurnitureRemoving : MonoBehaviour {
     [SerializeField][Range(0,1)] private float giveBackPercentage = .5f;
+    private FurniturePlacement placement;
+    private Wall[] walls;
+
+    private void Awake() {
+        placement = GetComponentInParent<FurniturePlacement>();
+        walls = FindObjectsOfType<Wall>();
+    }
 
     private void OnEnable() {
         PlayerInput.Remove += Remove;
@@ -19,6 +26,8 @@ public class FurnitureRemoving : MonoBehaviour {
                 furniture.SpawnParticle(furniture.Settings.DestroyParticlePrefab);
                 CostText.CurrentMaterial += furniture.Cost * giveBackPercentage;
                 Door.CurrentFurnitures.Remove(furniture.CustomName);
+                Destroy(placement.Furniture);
+                for (int i = 0; i < walls.Length; i++) walls[i].ChangeMaterialToOriginal();
                 Destroy(furniture.gameObject);
             }
             catch { }
