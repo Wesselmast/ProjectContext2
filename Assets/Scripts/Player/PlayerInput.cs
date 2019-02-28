680df1a9e90c32f5e3d288420e6d82483aee271a
Wesselmast <mastwessel@gmail.com>
2019-02-28 16:16:18 +0100

    Switch Scenes For No UI

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -14,6 +14,7 @@ namespace ContextInput {
         public static event Action Remove = delegate { };
         public static bool Place { get; private set; }
         public static Vector3 MousePosition { get { return Input.mousePosition; } }
+        public static event Action<int> SwitchScenes = delegate { };
 
         private void Update() {
             if (Input.GetKeyDown(KeyCode.Q)) Rotate(Direction.Left);
@@ -22,9 +23,13 @@ namespace ContextInput {
             else if (Input.GetKeyDown(KeyCode.D)) Move(Direction.Right);
             else if (Input.GetKeyDown(KeyCode.W)) Move(Direction.Forward);
             else if (Input.GetKeyDown(KeyCode.S)) Move(Direction.Back);
+            else if (Input.GetKeyDown(KeyCode.Alpha1)) SwitchScenes(0);
+            else if (Input.GetKeyDown(KeyCode.Alpha2)) SwitchScenes(1);
+            else if (Input.GetKeyDown(KeyCode.Alpha3)) SwitchScenes(2);
+            else if (Input.GetKeyDown(KeyCode.Alpha4)) SwitchScenes(3);
             else if (Input.GetKeyDown(KeyCode.R)) Remove();
-            //else if (Input.GetKeyDown(KeyCode.Delete)) Reset();
             else Place = Input.GetKeyDown(KeyCode.Space);
-        }   
+
+        }
     }
 }
