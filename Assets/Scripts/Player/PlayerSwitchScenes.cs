4f5c058f4b437acdd31e4df0619e8b9ec9017dce
Wesselmast <mastwessel@gmail.com>
2019-02-28 16:16:43 +0100

    Switch Scenes

---
+++ Assets/Scripts/Player/PlayerSwitchScenes.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using ContextInput;
+using UnityEngine.SceneManagement;
+
+public class PlayerSwitchScenes : MonoBehaviour {
+    private void OnEnable() {
+        PlayerInput.SwitchScenes += SceneSwitch;
+    }
+
+    private void OnDisable() {
+        PlayerInput.SwitchScenes -= SceneSwitch;
+    }
+
+    private void SceneSwitch(int index) {
+        SceneManager.LoadScene(index+1);
+    }
+}
