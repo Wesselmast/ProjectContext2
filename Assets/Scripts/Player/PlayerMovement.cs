27f3260ca9434bcbe246af90f2df933c19936a0f
Wesselmast <mastwessel@gmail.com>
2019-03-25 01:05:37 +0100

    4 directional ambigious movement + idle animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,9 +1,12 @@
 ﻿using ContextInput;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
     [SerializeField] private bool moveLocal;
+    [SerializeField] private float speed;
+    [SerializeField] private Animator characterAnimator;
     private Animator glowLeft;
     private Animator glowRight;
     private Animator glowTop;
@@ -14,6 +17,7 @@ public class PlayerMovement : MonoBehaviour {
     private Transform door;
 
     private FMODUnity.StudioEventEmitter ac;
+    private bool coroutineRunning = false;
 
     private void Awake() {
         glowLeft = GameObject.Find("SideGlowLeft").GetComponent<Animator>();
@@ -58,10 +62,10 @@ public class PlayerMovement : MonoBehaviour {
     private void CheckHowToMove(bool gun, bool self, Vector3 direction) {
         Vector3 prevPosition = transform.position;
         if (!Door.GunGone) {
-            if (gun && self) transform.position += direction;
+            if (gun && self && !coroutineRunning) if (!coroutineRunning) StartCoroutine(MoveToLocation(direction));
         }
-        else if (self) transform.position += direction;
-        if (prevPosition == transform.position) {
+        else if (self) if(!coroutineRunning) StartCoroutine(MoveToLocation(direction));
+        if (prevPosition == transform.position && !coroutineRunning) {
             try { ac.Play(); }
             catch { }
             if (direction == transform.right) glowTop.Play("SideGlowNew");
@@ -69,5 +73,27 @@ public class PlayerMovement : MonoBehaviour {
             else if (direction == -transform.forward) glowRight.Play("SideGlowNew");
             else glowLeft.Play("SideGlowNew");
         }
+    }
+
+    private IEnumerator MoveToLocation(Vector3 direction) {
+        coroutineRunning = true;
+        if (direction == transform.right) characterAnimator.SetBool("doWalkForwards", true);
+        else if (direction == -transform.right) characterAnimator.SetBool("doWalkBackwards", true);
+        else if (direction == -transform.forward) characterAnimator.SetBool("doWalkRight", true);
+        else characterAnimator.SetBool("doWalkLeft", true);
+        Vector3 startPos = transform.position;
+        float step = (speed / (startPos - (startPos + direction)).magnitude * Time.deltaTime);
+        float currentTime = 0;
+        while(currentTime <= 1f) {
+            currentTime += step;
+            transform.position = Vector3.Lerp(startPos, (startPos + direction), currentTime);
+            yield return null;
+        }
+        transform.position = startPos + direction;
+        characterAnimator.SetBool("doWalkForwards", false);
+        characterAnimator.SetBool("doWalkBackwards", false);
+        characterAnimator.SetBool("doWalkRight", false);
+        characterAnimator.SetBool("doWalkLeft", false);
+        coroutineRunning = false;
     }
 }
