9322627bd8877b7fd0311f5c8e5e266222eab6bd
Wesselmast <mastwessel@gmail.com>
2019-03-25 19:24:54 +0100

    New animations

---
+++ Assets/Scripts/Rest/Protester.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Protester : MonoBehaviour {
+
+    private Animator animator;
+    private float elapsed = 0;
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        if (elapsed <= 0) {
+            animator.SetFloat("ChanceToSwitch", Random.Range(0f, 1f));
+            elapsed = Random.Range(1f, 5f);
+        }
+        else elapsed -= Time.deltaTime;
+    }
+}
